fb27d830a849d0549a9bbae58da7f935cd0c84ba
EvenAR <even.rognlien@gmail.com>
2017-08-12 11:15:35 +0200

    Added pre-built binary

---
+++ index.js
@@ -0,0 +1,17 @@
+var nodeSimconnect = null;
+
+if(process.platform == "win32" && process.arch == "ia32") {
+	try {
+		// Try loading manual build first
+		nodeSimconnect = require('./build/Release/node-simconnect');
+	} catch(ex) {
+		// If it fails, load the included binary
+		nodeSimconnect = require('./bin/win_ia32/node-simconnect');
+	}
+} else if(process.platform == "win32" && process.arch == "x64") {
+	console.log(new Error("Node.js 64 bit is not supported. Please install Node.js 32 bit."))
+} else if(process.platform != "win32") {
+    console.log(new Error("The only supported platform is Windows (win32). Found: " + process.platform))
+}
+
+module.exports = nodeSimconnect;
