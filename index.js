29ab7e6082c5f3cb5be343159a1b1ad9617d2a39
EvenAR <even.rognlien@gmail.com>
2020-08-24 22:05:58 +0200

    Clearify output text from index.js

--- index.js
+++ index.js
@@ -9,54 +9,55 @@ try {
     console.info(`node-simconnect: Manual build loaded`);
 } catch (ex) {
     // If it fails, load the included binary
+    console.info(`node-simconnect: Failed to load manual build (${ex.code})`);
     console.info(`node-simconnect: Loading pre-built binary`);
     if (process.arch === `x64`) {
         throw `node-simconnect: The pre-built binary only works with the 32-bit version of Node.js`;
     }
     nodeSimconnect = require(`./bin/win_ia32/node-simconnect`);
-    console.info(`node-simconnect: Pre-built binary loaded: ${ex}`);
+    console.info(`node-simconnect: Pre-built binary loaded`);
 }
 
 nodeSimconnect.objectId = {
-	USER: 0
+    USER: 0
 };
 
 nodeSimconnect.dataRequestFlag = {
-	DEFAULT: 0,
-	CHANGED: 1,
-	TAGGED: 2
+    DEFAULT: 0,
+    CHANGED: 1,
+    TAGGED: 2
 };
 
 nodeSimconnect.datatype = {
-	INVALID: 0,  
-	INT32: 1,    
-	INT64: 2,    
-	FLOAT32: 3,  
-	FLOAT64: 4,  
-	STRING8: 5,  
-	STRING32: 6, 
-	STRING64: 7, 
-	STRING128: 8,
-	STRING256: 9,
-	STRING260: 10,
-	STRINGV: 11
+    INVALID: 0,  
+    INT32: 1,    
+    INT64: 2,    
+    FLOAT32: 3,  
+    FLOAT64: 4,  
+    STRING8: 5,  
+    STRING32: 6, 
+    STRING64: 7, 
+    STRING128: 8,
+    STRING256: 9,
+    STRING260: 10,
+    STRINGV: 11
 };
 
 nodeSimconnect.simobjectType = {
-	USER: 0,
-	ALL: 1,
-	AIRCRAFT: 2,
-	HELICOPTER: 3,
-	BOAT: 4,
-	GROUND: 5,
+    USER: 0,
+    ALL: 1,
+    AIRCRAFT: 2,
+    HELICOPTER: 3,
+    BOAT: 4,
+    GROUND: 5,
 };
 
 nodeSimconnect.period = {
-	NEVER: 0,
-	ONCE: 1,
-	VISUAL_FRAME: 2,
-	SIM_FRAME: 3,
-	SECOND: 4,
+    NEVER: 0,
+    ONCE: 1,
+    VISUAL_FRAME: 2,
+    SIM_FRAME: 3,
+    SECOND: 4,
 };
 
 
