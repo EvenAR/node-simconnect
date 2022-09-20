f272aa109326c942342c9fca8818c780bef2e688
EvenAR <even.rognlien@gmail.com>
2022-09-20 23:14:46 +0200

    Delete files from old version

--- scripts/install.js
+++
@@ -1,6 +0,0 @@
-const NOT_WINDOWS = process.platform !== "win32";
-const YELLOW = '\x1b[33m%s\x1b[0m';
-
-if (NOT_WINDOWS) {
-    console.log(YELLOW, `Warning: node-simconnect does not work on ${process.platform}`);
-}
