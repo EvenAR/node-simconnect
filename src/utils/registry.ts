c3ba401655817c406b3ab47d5514d32f966186c6
olli <65198941+knokbak@users.noreply.github.com>
2022-12-10 00:06:24 +0000

    fix undefined property access

--- src/utils/registry.ts
+++ src/utils/registry.ts
@@ -1,14 +1,18 @@
 // @ts-ignore // eslint-disable-line
 import regedit = require('regedit');
 
-export function readRegistryValue(FS_KEY: string, subKey: string): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
+/**
+ * Returns `undefined` if the key does not exist.
+ */
+export function readRegistryValue(FS_KEY: string, subKey: string): Promise<string | undefined> {
+    return new Promise<string | undefined>((resolve, reject) => {
         // eslint-disable-next-line
         regedit.list(FS_KEY, (err: any, result: any) => {
             if (err) {
                 reject(new Error(`Failed to read registry value ${FS_KEY} (${err})`));
             } else {
-                resolve(result[FS_KEY].values[subKey].value);
+                const values: { [key: string]: any } = result[FS_KEY]?.values;
+                resolve(values ? values[subKey].value : undefined);
             }
         });
     });
